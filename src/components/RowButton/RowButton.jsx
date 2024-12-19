77ecd96f3253fe2d9583934b99f8e8093045dbb6
cesar <cesarhernanruscica@gmail.com>
2024-12-19 12:04:50 -0300

    changed the styles for better look on mobile

--- src/components/RowButton/RowButton.jsx
+++ src/components/RowButton/RowButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styles from './RowButton.module.css';
+import { FiCornerDownRight } from "react-icons/fi";
 
 function RowButton({ info='info', details='', children }) {
   return (
     <Link to='/' className={styles.container}>
       <div className={styles.info}>
-        <span>{info}</span>
+        <span><FiCornerDownRight /> {info}</span>
         {(details !== '') ? <span className={styles.infoDetails}>{details}</span> : ''}
       </div>
       {children}
