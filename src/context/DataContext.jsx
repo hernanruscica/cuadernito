0d35bdda9f455a1c2bd231a79a26a92743d2d2a0
cesar <cesarhernanruscica@gmail.com>
2024-12-24 19:47:12 -0300

    added the header with two btns - added them and userSettings to the context

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -10,8 +10,8 @@ const initialState = {
 };
 
 const DataContext = createContext(initialState);
-//const publicUrl = 'https://cuadernito.onrender.com';
-const publicUrl = 'http://localhost:5173';
+const publicUrl = 'https://cuadernito.onrender.com';
+//const publicUrl = 'http://localhost:5173';
 
 const DataProvider = ({ children }) => {
   const [data, setData] = useState(initialState); // Use a single state for all data
