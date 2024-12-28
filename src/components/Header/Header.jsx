2b13d830ec08b7f62a5054f49190448d8cb8d619
cesar <cesarhernanruscica@gmail.com>
2024-12-28 17:50:07 -0300

    added favicon and logo.png with 50% opacity

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import styles from './Header.module.css'
 
-function Header({ title, subtitle }) {
-  //const imageSiteUrl = 'http://localhost:5173/images';
-  const imageSiteUrl = 'https://cuadernito.onrender.com/images';
+function Header({ title, subtitle }) {  
+  const imageSiteUrl = import.meta.env.VITE_IMAGE_DIRECTORY;  
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerText}>
         <h1 className={styles.headerTitle}>{title}</h1>        
         <p className={styles.headerSubtitle}>{subtitle}</p>     
       </div>
-      <img src={`${imageSiteUrl}/Cuadernitoapp.png`} alt="Cuadernito app" title="Cuadernito app"  className={styles.headerImage}/>
+      <img src={`${imageSiteUrl}/Cuadernitoapp50opacity.png`} alt="Cuadernito app" title="Cuadernito app"  className={styles.headerImage}/>
     </div>
   );
 }
